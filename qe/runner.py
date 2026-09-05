befe841a9cdc3afeff9440d1c317a6d4b28a3a7b
mjyazar <mjanjua.ed@gmail.com>
2026-09-05 16:03:02 +1000

    displays process duration counter

--- qe/runner.py
+++ qe/runner.py
@@ -1,4 +1,5 @@
-import subprocess   
+import subprocess
+import time
 
 NPROC = 4
 
@@ -9,4 +10,18 @@ def run(process, input_path, output_path):
     """
     
     with open(input_path, mode="r") as input_file, open(output_path, mode="w") as output_file:
-        subprocess.run(["mpirun", "-np", str(NPROC), process], stdin=input_file, stdout=output_file, check=True)
+        
+        start = time.perf_counter()
+        
+        calculation = subprocess.Popen(["mpirun", "-np", str(NPROC), process], stdin=input_file, stdout=output_file)
+
+        while calculation.poll() is None:
+            elapsed = int(time.perf_counter() - start)
+
+            hours = elapsed // 3600
+            minutes = (elapsed % 3600) // 60
+            seconds = elapsed % 60
+
+            print(f"\rElapsed: {hours:02d}:{minutes:02d}:{seconds:02d}", end="", flush=True)
+
+            time.sleep(1)
