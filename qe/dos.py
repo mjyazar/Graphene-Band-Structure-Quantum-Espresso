04815c9014392317f8b1223efa5faf136c1a9548
mjyazar <mjanjua.ed@gmail.com>
2026-09-07 11:40:19 +1000

    change degauss

--- qe/dos.py
+++ qe/dos.py
@@ -11,7 +11,7 @@ def write_input(input_path, outdir, data_path, prefix):
                          outdir = '{outdir}'  ! directory containing the input data, i.e. the pw.x metadata
                          bz_sum = 'smearing'  ! integration using gaussian smearing
                          ngauss = 0  ! type of gaussian broadening - 0: Simple Gaussian (default)
-                         degauss = 0.005  ! gaussian broadening, Ry (not eV!)
+                         degauss = 0.05  ! gaussian broadening, Ry (not eV!)
                          emin = -10
                          emax = 10
                          deltae = 0.01  ! energy grid step (eV)
