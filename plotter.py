b2c983bcf0e157e1f55208926246b2544a7a8a29
mjyazar <mjanjua.ed@gmail.com>
2026-09-15 14:22:22 +1000

    dir fix

--- plotter.py
+++ plotter.py
@@ -142,7 +142,7 @@ def subtracted_potential(results:Results, field):
     ax.set_ylabel("Potential Energy (eV)")
     ax.set_title(f"Subtracted Potential — E-field = {field}au")
 
-    plt.savefig(FIG_DIR / f"Subtracted Potential {field}au.png", dpi=300, bbox_inches="tight")
+    plt.savefig(POTENTIAL_DIR / f"Subtracted Potential {field}au.png", dpi=300, bbox_inches="tight")
     plt.close(fig)
     
 
