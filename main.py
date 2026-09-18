c8bd5afaee20e16f86a2f796b2869bc2a645f9c0
mjyazar <mjanjua.ed@gmail.com>
2026-09-18 20:30:50 +1000

    run qe=True

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ OUT_DIR.mkdir(exist_ok=True)
 
 # RUN_QE may be False if script already ran and want to work with existing files
 # True if running for the first time or want to create new files with new parameters
-RUN_QE = False
+RUN_QE = True
 RUN_LDOS = True
 RUN_CONVERGENCE = False
 
